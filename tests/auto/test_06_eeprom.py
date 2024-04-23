961778dfc2f7f63eafa1f0df67a62cb6c561e6fb
xudong <dong.xu@spacemit.com>
2024-04-23 21:31:59 +0800

    eeprom: modify the eeprom testcase
    Signed-off-by: xudong <dong.xu@spacemit.com>

--- tests/auto/test_06_eeprom.py
+++ tests/auto/test_06_eeprom.py
@@ -7,21 +7,23 @@ class EEPROM_Test(TestCase):
     LANGUAGES = {
         'zh': {
             'EEPROM_Test': 'IIC EEPROM',
-            'test_iic_eeprom': '读取数据'
+            'test_iic_eeprom': '检测设备'
         },
         'en': {
             'EEPROM_Test': 'IIC EEPROM',
-            'test_iic_eeprom': 'read data'
+            'test_iic_eeprom': 'detect device'
         }
     }
 
     def test_iic_eeprom(self):
         # 执行i2cget命令，读取I2C设备的数据
-        result = subprocess.run(['i2cget', '-y', '2', '0x50', '0x01'], capture_output=True, text=True)
+        # result = subprocess.run(['i2cget', '-y', '2', '0x50', '0x01'], capture_output=True, text=True)
         
         # 验证命令执行结果是否成功
-        self.assertEqual(result.returncode, 0)
+        # self.assertEqual(result.returncode, 0)
         
         # 验证读取到的数据是否符合预期
-        expected_data = '0x6c'  # 假设期望读取到的数据是0x6c
-        self.assertEqual(result.stdout.strip(), expected_data)
+        # expected_data = '0x6c'  # 假设期望读取到的数据是0x6c
+        # self.assertEqual(result.stdout.strip(), expected_data)
+        dev_file = '/sys/class/i2c-adapter/i2c-2/2-0050'
+        self.assertTrue(os.path.exists(dev_file))
